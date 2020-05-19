2e79455af4783087ce6cb9dc3b23e2af5fd52125
Uric <uricbonatti@gmail.com>
2020-05-19 14:50:06 -0300

    Calculadora - React

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button.css';
+
+// eslint-disable-next-line react/display-name
+export default (props) => {
+	let classes = 'button ';
+	classes += props.operation ? 'operation ' : '';
+	classes += props.double ? 'double' : '';
+	classes += props.triple ? 'triple' : '';
+	return (
+		<button
+			onClick={(e) => props.click && props.click(props.label)}
+			className={classes}
+		>
+			{props.label}
+		</button>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.Fragment>
+		<h1>Calculadora</h1>
+		<Calculator />
+	</React.Fragment>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
